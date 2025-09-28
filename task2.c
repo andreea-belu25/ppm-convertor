0726614c632b852061a3df22f9bc7fb09c0c0560
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 19:39:26 +0300

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,30 +1,31 @@
-/*BELU ANDREEA-DANIELA 314CB*/
 #include "header.h"
 
 TCoada InitQ(void)
 {
 	TCoada c = (TCoada)calloc(1, sizeof(TCelulaCoada));
-	 //  verific daca a reusit alocarea
+	// Check if allocation succeeded
 	if (!c)
 		return NULL;
-	// pointer catre coada daca a reusit alocarea
+	// Pointer to queue if allocation succeeded
 	return c;
 }
+
 void InsQ(TCoada c, TArb x)
 {
-	//  vreau sa inserez intr-o coada => aloc o celula noua pentru coada
+	// I want to insert in a queue => allocate a new cell for queue
 	TLista aux1 = (TLista)calloc(1, sizeof(TCelula));
-	//  verific alocarea
+	// Check allocation
 	if (!aux1)
 		return;
-	//  info e de tip TArb => trebuie alocata
+		
+	// Info is of type TArb => needs to be allocated
 	aux1->info = (TArb)calloc(1, sizeof(TNod));
 	if (!aux1->info) {
 		free(aux1);
 		return;
 	}
-	//  daca au reusit alocarile => copiez inf in celula pe care vreau
-	//  sa o adaug in coada
+	
+	// If allocations succeeded => copy info in cell I want to add to queue
 	aux1->info->dim = x->dim;
 	aux1->info->fiu1 = x->fiu1;
 	aux1->info->fiu2 = x->fiu2;
@@ -34,42 +35,51 @@ void InsQ(TCoada c, TArb x)
 	aux1->info->info.red = x->info.red;
 	aux1->info->info.green = x->info.green;
 	aux1->info->info.blue = x->info.blue;
-	//  e prima celula pe care urmeaza sa o adaug
+	
+	// It's the first cell I'm about to add
 	if (c->inc == NULL)
 		c->inc = aux1;
-	//  nu e prima celula inserata => o leg la ultima celula din coada
+	// It's not the first cell inserted => I link it to last cell in queue
 	else
 		c->sf->urm = aux1;
-	//  actualizez pointerul sf
+		
+	// Update the sf pointer
 	c->sf = aux1;
 }
+
 TLista ExtQ(TCoada c)
 {
-	//  nu am valori de extras
+	// No values to extract
 	if (c->inc == NULL)
 		return NULL;
-	//  extrag prima celula din coada
+		
+	// Extract first cell from queue
 	TLista aux = c->inc;
-	//  modificare varf stiva
+	// Modify queue front
 	c->inc = c->inc->urm;
-	//  o singura valoare in coada = > coada vida
+	
+	// Single value in queue => empty queue
 	if (c->inc == NULL) {
 		c->sf = NULL;
 	}
+	
 	return aux;
 }
-//  cand nu mai am nevoie de coada => o distrug
+
+// When I no longer need the queue => I destroy it
 void DistrQ(TCoada c)
 {
 	TLista p = c->inc, aux = c->inc;
-	//  parcurg coada
+	
+	// Traverse queue
 	while (p) {
 		aux = p;
 		p = p->urm;
 		free(aux->info);
 		free(aux);
 	}
+	
 	free(c);
-	//  ma asigur ca a devenit NULL pointerul catre coada
+	// I ensure the pointer to queue became NULL
 	c = NULL;
 }
