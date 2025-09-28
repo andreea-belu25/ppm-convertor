2daa589d69ea51ec09bc07d2f40ece7ac4b31457
Andreea-Daniela Belu <119062819+andreea-belu25@users.noreply.github.com>
2025-09-28 19:34:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,49 +1,54 @@
-/*BELU ANDREEA-DANIELA 314CB*/
 #include "header.h"
 
-//  vector de stringuri constante
-//  *argv[] = vector alocat dinamic cu dim variabila
 int main(int argc, const char *argv[])
 {
 	PPMImage *img = NULL;
-	// nu il pot initializa cu null pentru ca e pointer dublu
 	PPMPixel **grid;
 	int comp_color = 0;
 	char char1 = 'a', char2 = 'b';
-	//  factorul de compresie = argv[2] (char)
-	//  vreau sa il convertesc la numar intreg
 	int factor = 0;
+	
 	for (int i = 0; i < strlen(argv[2]); i++)
 		factor = factor * 10 + (argv[2][i] - '0');
+
 	if (strcmp(argv[1], "-c1") == 0 || strcmp(argv[1], "-c2") == 0) {
-		// argv[0] - e mereu executabilul, argv[3] = fisier_input
+		// argv[0] - is always the executable, argv[3] = input_file
 		FILE *read = fopen(argv[3], "rb");
+		
 		if (!read)
 			return 0;
-		//  citire format imagine (P6)
+		
+		// read image format (P6)
 		fscanf(read, "%c%c\n", &char1, &char2);
-		//  verificare imagine format
+		
+		// verify image format
 		if (char1 != 'P' || char2 != '6')
 			return 0;
-		// aloc memorie pentru imagine
+		
+		// allocate memory for image
 		img = (PPMImage *)calloc(1, sizeof(PPMImage));
 		if (!img)
 			return 0;
-		//  citire dimensiuni imagine
+		
+		// read image dimensions
 		fscanf(read, "%d %d\n", &img->width, &img->height);
+		
 		fscanf(read, "%d", &comp_color);
 		char random = '\0';
 		fread(&random, sizeof(char), 1, read);
-		// alocare matrice de pixeli
+		
+		// allocate pixel matrix
 		grid = (PPMPixel **)calloc(img->height, sizeof(PPMPixel *));
 		if (!grid)
 			return 0;
+		
 		for (int i = 0; i < img->height; i++) {
 			grid[i] = (PPMPixel *)calloc(img->width, sizeof(PPMPixel));
 			if (!grid[i])
 				return 0;
 		}
-		//  citesc imaginea
+		
+		// read the image
 		for (int i = 0; i < img->width; i++)
 			for (int j = 0; j < img->height; j++) {
 				fread(&grid[i][j].red, sizeof(unsigned char), 1, read);
@@ -52,66 +57,81 @@ int main(int argc, const char *argv[])
 			}
 		fclose(read);
 	}
+	
 	if (strcmp(argv[1], "-c1") == 0) {
-		// argv[0] - e mereu executabilul, argv[3] = fisier_input
+		// argv[0] - is always the executable, argv[3] = input_file
 		FILE *read = fopen(argv[3], "rb");
 		if (!read)
 			return 0;
-		//  construire arbore de compresie pe baza matricei date
+		
+		// build compression tree based on given matrix
 		TArb a = arb_compresie(0, 0, grid, img->width, factor);
 		int nr_niveluri = NrNiveluri(a);
 		int nr_frunze = Numara_Frunze(a);
 		int nivel_min_frunza = niv_min_frunza(a, 0);
-		//printf("lowest level %d\n", nivel_min_frunza);
 		int dim_frunza_niv_min = info_nod_max(a, nivel_min_frunza, 0);
-		//  afisare in fisier text
+		
 		FILE *write = fopen(argv[4], "w");
 		if (!write)
 			return 0;
-		//  afisari cerinta 1
+		
 		fprintf(write, "%d\n", nr_niveluri);
 		fprintf(write, "%d\n", nr_frunze);
 		fprintf(write, "%d\n", dim_frunza_niv_min);
-		//  alocare locala in primul if
+		
+		// local allocation in first if
 		for (int i = 0; i < img->width; i++)
 			free(grid[i]);
 		free(grid);
 		free(img);
-		//  distrug arborele de compresie format prin apel
+		
+		// destroy compression tree formed by call
 		distruge(a);
-		//  deschideri locale => inchideri locale
+		
+		// local openings => local closings
 		fclose(write);
 		fclose(read);
 	}
+	
 	if (strcmp(argv[1], "-c2") == 0) {
-		// argv[0] - e mereu executabilul, argv[3] = fisier_input
+		// argv[0] - is always the executable, argv[3] = input_file
 		FILE *read = fopen(argv[3], "rb");
 		if (!read)
 			return 0;
+		
 		FILE *write = fopen(argv[4], "wb");
 		if (!write)
 			return 0;
-		//  dim imagine
+		
+		// image dimensions
 		fwrite(&img->width, sizeof(unsigned int), 1, write);
-		//  creare arbore de compresie
+		
+		// create compression tree
 		TArb a = arb_compresie(0, 0, grid, img->width, factor);
-		//  afisare arbore in latime folosind o coada
+		
+		// display tree in breadth-first order using a queue
 		TCoada c = InitQ();
 		InsQ(c, a);
 		TLista qStart = c->inc;
+		
 		while (c->inc != NULL) {
-			//  inserarea se face doar daca nu sunt nuli, altfel da seg fault
+			// insertion is done only if they are not null, otherwise it causes seg fault
 			if (c->inc->info->fiu1)
 				InsQ(c, c->inc->info->fiu1);
+			
 			if (c->inc->info->fiu2)
 				InsQ(c, c->inc->info->fiu2);
+			
 			if (c->inc->info->fiu3)
 				InsQ(c, c->inc->info->fiu3);
+			
 			if (c->inc->info->fiu4)
 				InsQ(c, c->inc->info->fiu4);
+			
 			TLista cell = ExtQ(c);
 			TArb aux = cell->info;
 			char tip = 0;
+			
 			if (aux->type == 1) {
 				unsigned char r = (unsigned char)aux->info.red;
 				unsigned char g = (unsigned char)aux->info.green;
@@ -125,54 +145,70 @@ int main(int argc, const char *argv[])
 				fwrite(&tip, sizeof(unsigned char), 1, write);
 			}
 		}
+		
 		c->inc = qStart;
-		// distruge coada necesara parcurgerii in latime
+		
+		// destroy queue needed for breadth-first traversal
 		DistrQ(c);
-		//  distruge arborele creat prin comprsie
+		
+		// destroy tree created by compression
 		distruge(a);
-		//  alocari si deschideri locale => dezalocari si inchideri locale
+		
+		// local allocations and openings => local deallocations and closings
 		for (int i = 0; i < img->width; i++)
 			free(grid[i]);
 		free(grid);
 		free(img);
+		
 		fclose(write);
 		fclose(read);
 	}
+	
 	if (strcmp(argv[1], "-d") == 0) {
-		// argv[0] - e mereu executabilul, argv[2] = fisier_input
+		// argv[0] - is always the executable, argv[2] = input_file
 		FILE *read = fopen(argv[2], "rb");
 		if (!read)
 			return 0;
+		
 		unsigned int dim = 0;
 		fread(&dim, sizeof(unsigned int), 1, read);
+		
 		grid = (PPMPixel **)calloc(dim, sizeof(PPMPixel *));
 		if (!grid)
 			return 0;
+		
 		for (int i = 0; i < dim; i++) {
 			grid[i] = (PPMPixel *)calloc(dim, sizeof(PPMPixel));
 			if (!grid[i])
 				return 0;
 		}
-		//  initializare coada pixeli + construire
+		
+		// initialize pixel queue + construction
 		TCoada_pixel c = InitQ_pixel();
 		construieste_coada_pixeli(c, read);
 		TArb arb = NULL;
-		//  parcurg arb de mai multe ori pana la frunze
-		//  pt ca fct de recursivitate creeaza doar un sg nivel
+		
+		// traverse tree multiple times until leaves
+		// because recursion function creates only one level
 		while (c->inc != NULL) {
 			arb = construieste_arbore(c, arb);
 		}
+		
 		Parcurge_Arb(arb, grid, 0, 0, dim);
 		afisare_imagine(grid, dim, argv[3]);
-		// alocari locale => dezalocari locale
+		
+		// local allocations => local deallocations
 		for (int i = 0; i < dim; i++)
 			free(grid[i]);
 		free(grid);
 		free(img);
+		
 		DistrugereQ(c);
 		distruge(arb);
-		//  deschidere locala => inchidere locala
+		
+		// local opening => local closing
 		fclose(read);
 	}
+	
 	return 0;
 }
